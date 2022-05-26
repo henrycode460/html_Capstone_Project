adb8ce6486bef5462021ac5b41d2793c48f775c7
Henry <dokinruss@gmail.com>
2022-05-26 23:37:39 +0000

    Add script to create an extra features

--- script/app.js
+++ script/app.js
@@ -73,3 +73,92 @@ window.onload = () => {
     featureSpeaker ();
   };
   
+
+//   extra feature
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+
+const giveaway = document.querySelector(".course_time");
+const deadline = document.querySelector(".deadline");
+const item = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2022,4,31,11,13,40);
+
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minute = futureDate.getMinutes();
+const date = futureDate.getDate();
+
+
+
+let month = futureDate.getMonth();
+month = months[month];
+
+let day = futureDate.getDay();
+day = weekdays[day];
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime (){
+  const today = new Date().getTime();
+  const t = futureDate - today;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const onehour = 60 * 60 * 1000; 
+  const oneminute = 60 * 1000;
+
+  let days = t / oneDay;
+  days = Math.floor(days); 
+
+  let hours = Math.floor((t % oneDay) / onehour);
+
+  let minutes = Math.floor((t % onehour) / oneminute);
+  let seconds = Math.floor((t % oneminute) / 1000);
+
+  const values = [days,hours,minutes,seconds];
+
+function format (item){
+  if(item < 10 ){
+    return item = `0${item}`
+  }
+  return item;
+}
+
+item.forEach(function(item, index){
+item.innerHTML = format(values[index]); 
+
+});
+  if(t < 0){
+    clearInterval(countDown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, This promotion has expired!!!</h4>`
+  }
+
+}
+
+let countDown = setInterval(getRemainingTime, 1000)
+
+getRemainingTime()
