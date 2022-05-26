1997b3523ca549f67e4116c464a3049b58cf94cc
Henry <dokinruss@gmail.com>
2022-05-26 15:17:43 +0000

    Add script to display feature speaker from a javascript object

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+
 // navigation script
 const navOpenBtn = document.querySelector("#hambugerBtn");
 const navCloseBtn = document.querySelector("#btnClose");
@@ -23,3 +24,52 @@ navCloseBtn.addEventListener("click", function(){
     document.getElementById("hambugerBtn").style.visibility = "visible";
 
 })
+
+// feature speaker section
+const featureSpeaker = () =>{
+    const cardDetails = featureCardDetails;
+    cardDetails .forEach((data) => {
+        const featureSpeakerCardWrapper = document.querySelector(".speaker-Card-Wrapper")
+        
+        const speakerCardSec = document.querySelector('.speakerCard-sec')
+        const speakerCard = document.createElement('div');
+        const cardTextCont = document.createElement('div');
+        const featureSpeakerSec = document.querySelector(".featureSpeaker-section");
+        const featureSpeakerwrap = document.querySelector(".featureSpeaker-sec-wrapper");
+        const featurespeakHead = document.querySelector(".featureSpeaker_heading");
+        const featureSpeakertext = document.createElement('h1');
+        const SpeakerImg = document.createElement('img');
+        const speakerName = document.createElement('h2');
+        const speakerJob = document.createElement('h4');
+        const speakerJobDesc = document.createElement('p');
+        const speakerLine = document.createElement('hr');
+      
+
+        featureSpeakerCardWrapper.classList.add('speaker-Card-Wrapper');
+        speakerCardSec.classList.add('speakerCard-sec');
+        speakerCard.classList.add('speakerCard');
+        cardTextCont.classList.add('card-text-cont');
+        featureSpeakertext.classList.add('featureSpeaker_text');
+        SpeakerImg.classList.add('card_sec_img');
+        speakerName.classList.add('card-speaker_name');
+        speakerJob.classList.add('card_speaker-job');
+        speakerJobDesc.classList.add('speaker-job-desc');
+        SpeakerImg.setAttribute('src', data.speakerImage);
+        speakerName.innerHTML = data.speakerName;
+        speakerJob.innerHTML = data.speakerJob;
+        speakerJobDesc.innerText = data.speakerJobDesc;
+        cardTextCont.append(speakerName, speakerJob, speakerLine, speakerJobDesc);
+        speakerCard.append(SpeakerImg, cardTextCont);
+        speakerCardSec.append(speakerCard)
+        featureSpeakerCardWrapper.append(speakerCardSec);
+        featureSpeakerwrap.append(featurespeakHead, featureSpeakerCardWrapper);
+        featureSpeakerSec.append(featureSpeakerwrap);
+
+    });
+
+}
+
+window.onload = () => {
+    featureSpeaker ();
+  };
+  
---
+++ script/object.js
@@ -0,0 +1,50 @@
+const featureCardDetails = [
+    {
+      id: 1,
+      speakerName: 'Bernie Wanger',
+      speakerJob: 'Google Cloud Expert ',
+      speakerJobDesc: 'Cloud computing expert, and AI Specialist',
+      speakerImage: 'https://henrycode460.github.io/html_Capstone_Project/asset/images/yochai-benkler_first_speaker.png',
+      
+    },
+    {
+      id: 2,
+      speakerName: 'George Gatimu',
+      speakerJob: 'Cloud Modernization Expert ',
+      speakerJobDesc: 'Cloud computing Modernization Specialist',
+      speakerImage: 'https://henrycode460.github.io/html_Capstone_Project/asset/images/Kilnam-chon_speaker_Two.png',
+    },
+    {
+      id: 3,
+      speakerName: 'Laurie Pottmeyer',
+      speakerJob: 'Community Lead',
+      speakerJobDesc: 'Specalize in VR and Metaverse Designed',
+      speakerImage: 'https://henrycode460.github.io/html_Capstone_Project/asset/images/julia-leda_Third_Speaker.png',
+      
+    },
+    {
+      id: 4,
+      speakerName: 'Heather Newman',
+      speakerJob: 'Microsoft, USA, Principal PM Manager',
+      speakerJobDesc: 'Dynamics 365 & Power Platform Community Success',
+      speakerImage: 'https://henrycode460.github.io/html_Capstone_Project/asset/images/sohyeon-noh_speaker_four.png',
+      
+    },
+    {
+        id: 5,
+        speakerName: 'Tom Janetscheck',
+        speakerJob: 'Microsoft, Germany',
+        speakerJobDesc: 'Security Program Manager at Microsoft',
+        speakerImage: 'https://henrycode460.github.io/html_Capstone_Project/asset/images/ryan-merkley_fifth_speaker.png',
+        
+      },
+      {
+        id: 6,
+        speakerName: 'Annie Talvasto',
+        speakerJob: 'Microsoft Azure MVP, Swtzerland',
+        speakerJobDesc: 'Sr. Product Marketing Manager at Camunda',
+        speakerImage: 'https://henrycode460.github.io/html_Capstone_Project/asset/images/lila-tretikov_sixth_Speaker.png',
+        
+      }
+   
+  ];
